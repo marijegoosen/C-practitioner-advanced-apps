83fbee1e5561eedc32c2b980f5afe9bf6ec4e536
M. Goosen <marije_mh@live.nl>
2019-01-12 18:27:03 +0100

    added new project

---
+++ CarCompany version 2 (ASP Assignment 2)/CarCompany/CarCompany/Data/CarCompanyContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CarCompany.Models;
+
+namespace CarCompany.Models
+{
+    public class CarCompanyContext : DbContext
+    {
+        public CarCompanyContext (DbContextOptions<CarCompanyContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CarCompany.Models.Car> Car { get; set; }
+
+        public DbSet<CarCompany.Models.Company> Company { get; set; }
+
+        public DbSet<CarCompany.Models.Project> Project { get; set; }
+
+        public DbSet<CarCompany.Models.Skill> Skill { get; set; }
+
+        public DbSet<CarCompany.Models.User> User { get; set; }
+    }
+}
---
+++ CarCompany version 2 (ASP Assignment 2)/CarCompany/CarCompany/Models/Skill.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarCompany.Models
+{
+    public class Skill
+    {
+        public long SkillId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Company CompanyID { get; set; }
+    }
+}
