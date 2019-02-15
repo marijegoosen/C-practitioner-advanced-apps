9a9ae15e41b379878bce33f9688294f0c9b04e15
M. Goosen <marije_mh@live.nl>
2019-02-16 00:15:13 +0100

    added Enum

---
+++ AssignmentHans2 (spookhuis)/AssignmentHans2/Enumerations/EViewCommands.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HauntedHouse.Enumerations
+{
+    enum EViewCommands
+    {
+        status,
+        inventory,
+        ghost,
+        item
+    }
+}
